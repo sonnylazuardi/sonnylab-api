101c1921b3d2bb307833aaadafe0dbdea4c6f1c0
Sonny Lazuardi <sonnylazuardi@gmail.com>
2016-03-25 20:30:11 +0700

    add image replace compression

--- routes/phpindonesia.js
+++ routes/phpindonesia.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+var imageReplace = {
+    'http://www.event.phpindonesia.or.id/images/30jan2016_technology_update.jpg': 'https://lh3.googleusercontent.com/-zROEwpq1dOg/VvU8vFg1asI/AAAAAAAAKwI/4f_LWCuOyIkCJ3vEuxrMcW2cmaStMrfdA/s0/30jan2016_technology_update.jpg'
+};
+
 const PhpIndonesia = (app) => {
     app.get('/phpindonesia/event', (req, res) => {
         axios.get('http://www.event.phpindonesia.or.id/').then((result) => {
@@ -8,8 +12,9 @@ const PhpIndonesia = (app) => {
             var items = [];
             $('.items > .row').filter(function () {
                 var data = $(this);
-
-                console.log(data.find('table').first().children().first().children().eq(1).html());
+                var image = 'http://www.event.phpindonesia.or.id' + data.find('img').first().attr('src');
+                if (imageReplace.hasOwnProperty(image))
+                    image = imageReplace[image];
 
                 items.push({
                     title: data.find('h4').first().children().first().text(),
@@ -19,7 +24,7 @@ const PhpIndonesia = (app) => {
                     venue: data.find('table').first().children().eq(2).children().eq(1).text(),
                     seat: data.find('table').first().children().eq(3).children().eq(1).text(),
                     status: data.find('table').first().children().eq(4).children().eq(1).text(),
-                    image: 'http://www.event.phpindonesia.or.id' + data.find('img').first().attr('src')
+                    image: image
                 });
             });
             res.json(items);
